21974ba02659e341ec2718b3e7ad88fc6c0a7578
Alpimax <alperrozkann@gmail.com>
2022-08-26 15:47:17 -0400

    My java documents

---
+++ src/practice/Learning/Day39/FinalExample.java
@@ -0,0 +1,14 @@
+package practice.Learning.Day39;
+
+public class FinalExample {
+    public final int a = 10 ; // hard coded instance variable
+    public final int b ;
+
+    public static final
+
+    public FinalExample(int num ){
+        b=num; // when the object is created,
+        // the give argument will be the value will be the of b, it cannot be changed.
+    }
+
+}
