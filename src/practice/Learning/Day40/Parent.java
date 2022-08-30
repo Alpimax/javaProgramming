a0e8ce92de101cd2de9666a5ec746525b0b3b056
Alpimax <alperrozkann@gmail.com>
2022-08-30 10:22:36 -0400

    My java documents

---
+++ src/practice/Learning/Day40/Parent.java
@@ -0,0 +1,19 @@
+package practice.Learning.Day40;
+
+public class Parent {
+    public static void hello() {
+        System.out.println("Hello from Parent");
+    }
+}
+class Child extends Parent{
+//    @Override : Static variables cannot override
+    public static void hello() { // when I make this method, I am hiding the one from parent
+        System.out.println("Hello from Child");
+    }
+}
+class Run {
+    public static void main(String[] args) {
+        Parent.hello();
+        Child.hello();
+    }
+}
