8db83b5190d8b5963b075fd968d3314d2118eb55
Alpimax <alperrozkann@gmail.com>
2022-09-26 12:08:42 -0400

    My java documents

--- src/practice/Learning/Day37/Phone/AppleStore.java
+++ src/practice/Learning/Day37/Phone/AppleStore.java
@@ -2,10 +2,10 @@ package practice.Learning.Day37.Phone;
 
 public class AppleStore {
     public static void main(String[] args) {
-        Iphone iphone1 = new Iphone("Iphone X", "Black", 1000,256);
+        Iphone iphone1 = new Iphone("Iphone X", "Black", 1000, 256);
         System.out.println(iphone1);
         System.out.println("------------------------");
-        Iphone iphone2 = new Iphone("Iphone 13", "Silver", 1000,256);
+        Iphone iphone2 = new Iphone("Iphone 13", "Silver", 1000, 256);
         System.out.println(iphone2);
     }
 }
