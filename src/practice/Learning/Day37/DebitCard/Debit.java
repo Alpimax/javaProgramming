9b16b3a7463fda56e236e84a4b640039e130b655
Alpimax <alperrozkann@gmail.com>
2022-09-26 12:09:26 -0400

    My java documents

--- src/practice/Learning/Day37/DebitCard/Debit.java
+++ src/practice/Learning/Day37/DebitCard/Debit.java
@@ -2,7 +2,8 @@ package practice.Learning.Day37.DebitCard;
 
 public class Debit {
 
-    static  String accountType;
+    static String accountType;
+
     static {
         accountType = "Debit";
     }
