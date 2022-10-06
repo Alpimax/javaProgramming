ded17e3b5ccb56c16d36cc53294b982314f79500
Alpimax <alperrozkann@gmail.com>
2022-10-06 11:43:28 -0400

    My java documents

--- src/practice/StreamMethods.java
+++ src/practice/StreamMethods.java
@@ -1,8 +1,16 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StreamMethods {
 
     public static void main(String[] args) {
-        list<>
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(4, 6, 3, 36, 2,6));
+        list1.stream();
+        System.out.println(list1.stream().distinct().collect(Collectors.toList()));
+
     }
 }
